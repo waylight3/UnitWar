860a809212a534714a79baa58897adfa32106640
waylight3 <waylight3@naver.com>
2016-07-15 03:30:37 +0900

    0.0.2 update

--- Assets/Scripts/UserControl.cs
+++ Assets/Scripts/UserControl.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class UserControl : MonoBehaviour {
-	public GameObject cam;
-
+	public Camera cam;
+	public GameObject unit;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -11,12 +12,23 @@ public class UserControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis("Mouse ScrollWheel") > 0 && cam.transform.position.z > 10) {
+		if (Input.GetAxis("Mouse ScrollWheel") > 0 && cam.transform.position.y > 5) {
 			Vector3 temp = cam.transform.position;
-			cam.transform.position = temp + new Vector3(0, 0, -1);
-		} else if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+			cam.transform.position = temp + new Vector3(0, -1, 0);
+		} else if (Input.GetAxis("Mouse ScrollWheel") < 0 && cam.transform.position.y < 30) {
 			Vector3 temp = cam.transform.position;
-			cam.transform.position = temp + new Vector3(0, 0, 1);
+			cam.transform.position = temp + new Vector3(0, 1, 0);
 		}
+
+		if (Input.GetMouseButtonUp (0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if( Physics.Raycast(ray, out hitInfo, 100f) ) {
+                Debug.Log("hit point : " + hitInfo.point);
+				Vector3 newpos = hitInfo.point;
+				newpos.y = 0.2f;
+				Instantiate(unit, newpos, Quaternion.identity);
+            }
+        }
 	}
 }
--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # UnitWar
 ## What it is
-UnitWar is simple unit-base wargame. User can make type of unit and they will be used to fight with others.
+UnitWar is simple unit-base wargame. User can make and control type of unit and they will be used to fight with others.
 
 ## Info
 * Developer: waylight3 (waylight3@naver.com)
 * Tool: Unity (with C#)
-* Latest version: 0.0.1 (alpha)
+* Latest version: 0.0.2 (alpha)
 * Language support: Korean, English
 
 ## Update Log
-* 0.0.1
+* 0.0.1 (2016-07-14 22:33 KST)
  * Start project!
  * make basic UI
+* 0.0.2 (2016-07-15 03:28 KST)
+ * change objects from 2d to 3d
+ * add material kit
+ * set min-zoom(5) and max-zoom(30)
+ * change direction from z-axis to y-axis for apply gravity
